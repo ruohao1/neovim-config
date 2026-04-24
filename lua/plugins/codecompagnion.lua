f50d288ff1a7f4866a35ae82dcf5f084588ba2ab
ruohao <ruohaolin@gmail.com>
2026-04-23 22:59:17 -0400

    fix codecompanion adapter config

--- lua/plugins/codecompagnion.lua
+++ lua/plugins/codecompagnion.lua
@@ -3,6 +3,20 @@ return {
 		"olimorris/codecompanion.nvim",
 		version = "^18.0.0",
 		opts = {
+			adapters = {
+				acp = {
+					codex = function()
+						return require("codecompanion.adapters").extend("codex", {
+							defaults = {
+								auth_method = "openai-api-key", -- "openai-api-key"|"codex-api-key"|"chatgpt"
+							},
+							env = {
+								OPENAI_API_KEY = "OPENAI_API_KEY",
+							},
+						})
+					end,
+				},
+			},
 			interactions = {
 				chat = {
 					keymaps = {
@@ -12,7 +26,7 @@ return {
 			},
 		},
 		config = function(_, opts)
-			require("codecompagnion").setup(opts)
+			require("codecompanion").setup(opts)
 		end,
 		dependencies = {
 			"nvim-lua/plenary.nvim",
